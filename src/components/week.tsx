2823b6dd68cafff0118fa6a5427ff3858d059d69
Manel <manel.perez@agilogy.com>
2017-12-19 12:42:18 +0100

    create-react-app

---
+++ src/components/week.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+type State = {
+  selectedWeek: string
+};
+
+export class Week extends React.Component<any, State> {
+
+  render() {
+    return (
+      <div>
+        Hello world
+      </div>
+    );
+  }
+
+}
